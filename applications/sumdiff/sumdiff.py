21eed3f44892f4251f44c220e930e0d009df23c9
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-08-05 11:02:52 -0600

    Implement brute force solution to sumdiff

--- applications/sumdiff/sumdiff.py
+++ applications/sumdiff/sumdiff.py
@@ -2,14 +2,19 @@
 find all a, b, c, d in q such that
 f(a) + f(b) = f(c) - f(d)
 """
+import itertools
 
 #q = set(range(1, 10))
 #q = set(range(1, 200))
-q = (1, 3, 4, 7, 12)
+q = set([1, 3, 4, 7, 12])
 
 
 def f(x):
     return x * 4 + 6
 
-# Your code here
-
+for a in q:
+    for b in q:
+        for c in q:
+            for d in q:
+                if (f(a) + f(b)) == (f(c) - f(d)):
+                    print(f"f({a}) + f({b}) == f({c}) - f({d})    {f(a)} + {f(b)} == {f(c)} - {f(d)}")
